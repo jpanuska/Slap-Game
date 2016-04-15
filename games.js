65e84b81f3337e021ee8ab73b4b2cebca483371b
Jay Panuska <jay.panuska@gmail.com>
2016-04-15 13:00:32 -0600

    This test works 3 buttons

--- games.js
+++ games.js
@@ -1,8 +1,37 @@
-var health=100
 
-function(slap){
-    health=health-1
-    
-    
-    
+
+
+var health = 100
+
+function slap() {
+    health = health - 1;
+    alert(health);
+
 }
+
+
+
+function punch() {
+    health = health - 5;
+    alert(health);
+    //   update()
+}
+
+
+
+
+function kick() {
+    health = health - 10;
+    alert(health);
+    //   update()  
+
+}
+
+
+
+
+// function update()
+
+// document.getElementById
+
+  
